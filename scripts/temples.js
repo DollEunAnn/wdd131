7296acfa7f062b9678ab37ed6adf34f6e68d5e37
DollEunAnn <euniceannhdollete@gmail.com>
2024-09-28 11:45:46 +0800

    adjust min-width for large screen, update the button change for menu, fixed ending slash, implement css variable

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,1 +1,9 @@
 
+// get the hamburger icon element
+const hamburgerIcon = document.getElementById('hamburger');
+const navigation = document.getElementById('navMenu');
+
+hamburgerIcon.addEventListener('click', function () {
+    navigation.classList.toggle('show');
+    hamburgerIcon.classList.toggle('open');
+});
